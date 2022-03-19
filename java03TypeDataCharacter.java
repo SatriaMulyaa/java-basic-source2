8d901c4413434b1440c8537838e924a1f7bd4cc3
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-03-19 14:19:51 +0700

    Training

---
+++ java03TypeDataCharacter.java
@@ -0,0 +1,12 @@
+public class java03TypeDataCharacter {
+    public static void main(String[] args) {
+        char s = 's';
+        System.out.print(s);
+        char o = 'o';
+        System.out.print(o);
+        char l = 'l';
+        System.out.print(l);
+        char o2 = 'o';
+        System.out.print(o2);
+    }
+}
---
+++ java04TypeDataBoolean.java
@@ -0,0 +1,9 @@
+public class java04TypeDataBoolean {
+    public static void main(String[] args) {
+        boolean trues = true;
+        System.out.println(trues);
+
+        boolean falses = false;
+        System.out.println(falses);
+    }
+}
