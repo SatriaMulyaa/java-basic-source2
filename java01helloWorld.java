1be5869df71cf38ca0a8d9a7037eb915c1c2b158
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-03-19 13:56:46 +0700

    java Hello World

--- java01helloWorld.java
+++ java01helloWorld.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class java01helloWorld {
+public class java01helloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
