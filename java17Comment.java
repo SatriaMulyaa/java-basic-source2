25417ac88fd925c9c11fe06f0ef7a3a0a640c6ea
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-03-20 14:18:40 +0700

    Training

---
+++ java17Comment.java
@@ -0,0 +1,14 @@
+public class java17Comment {
+    public static void main(String[] args) {
+    }
+
+    /**
+     * method for addition variable1 and variable2
+     * @param value1
+     * @param value2
+     * @return result addition from variable 1 and variable 2
+     */
+    static int addtion(int value1, int value2){
+        return value1 + value2;
+    }
+}
